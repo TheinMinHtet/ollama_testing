128f28c497d4c9bdddc4f39143e3d0e50204310a
Thein Min Htet <152710449+TheinMinHtet@users.noreply.github.com>
2025-12-28 21:07:43 +0630

    Delete package.py

--- package.py
+++
@@ -1,9 +0,0 @@
-import ollama
-
-client = ollama.Client()
-model = "gemma3:1b"
-prompt = "What is Python?"
-response =  client.generate(model=model, prompt=prompt)
-
-print("Response from Ollama:")
-print(response.response)
